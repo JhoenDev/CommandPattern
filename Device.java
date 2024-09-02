b29fc7e251e3cf488a4e9f8f5382bd03fc0529b6
jhoen <jhoen.alban@gmail.com>
2024-09-02 18:41:37 +0800

    Updated Device.java

--- Device.java
+++ Device.java
@@ -1,3 +1,11 @@
 public abstract class Device {
+    String deviceName;
 
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
 }
